3538e952366c36f619e18c3013565a53c7e3667a
Mohammed Aftab <beingaftab008@gmail.com>
2026-01-14 00:28:03 +0530

    peakIndex in a Mounted Array

---
+++ C++ PROGRAMS/dsaProblems/firstAndLastOccurrence.cpp
@@ -0,0 +1,52 @@
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int first = firstTerm(nums, target);
+//         if (first == -1) return {-1, -1};
+        
+//         int last = lastTerm(nums, target);
+//         return {first, last};  
+//     }
+
+//     int firstTerm(vector<int>& nums, int target){
+//         int low = 0, high = nums.size() - 1;
+//         int ans = -1;
+
+//         while(low <= high){
+//             int mid = low + (high - low) / 2;
+
+//             if(nums[mid] == target){
+//                 ans = mid;          // FIX 1
+//                 high = mid - 1;     // move left
+//             }
+//             else if(nums[mid] < target){
+//                 low = mid + 1;
+//             }
+//             else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return ans;
+//     }
+
+//     int lastTerm(vector<int>& nums, int target){
+//         int low = 0, high = nums.size() - 1;
+//         int ans = -1;
+
+//         while(low <= high){
+//             int mid = low + (high - low) / 2;
+
+//             if(nums[mid] == target){
+//                 ans = mid;          // FIX 1
+//                 low = mid + 1;      // move right
+//             }
+//             else if(nums[mid] < target){
+//                 low = mid + 1;
+//             }
+//             else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
---
+++ C++ PROGRAMS/dsaProblems/peakIndex.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;    
+int main(){
+    int arr[5] = {1, 3, 7, 12, 9};
+}
